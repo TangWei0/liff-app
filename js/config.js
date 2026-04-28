bc150c873f21f3689ff428c147a836af2d9ffc7b
偉 湯 <toui860722@gmail.com>
2026-04-28 12:30:23 +0900

    GAS　ローカル環境　デプロイ更新される

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 ﻿const CONFIG = {
     LIFF_ID: "2009873872-wrcf23zq",
-    GAS_URL: "https://script.google.com/macros/s/AKfycbzHwwnfbGH6-QbnluId8uMWg1FpOaKX9jb__EhO7XIoNzaN4Kt41yYExP6QW68Pjxxu/exec"
+    GAS_URL: "https://script.google.com/macros/s/AKfycbw290itLDb_nVTeBT8q15-9S2XB6mCftQjfO9hRih3ki4eloFoQ6OYifnJHDaTr1gCz/exec"
 };
